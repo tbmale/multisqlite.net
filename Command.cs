5fd3abfdc64838f6fd2325a6193688d947378d39
ExtraVeral <tbmale@yahoo.com>
2018-02-04 16:20:06 +0100

    Command class added

---
+++ Command.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: io
+ * Date: 04/02/2018
+ * Time: 16:18
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace multisqlite
+{
+	/// <summary>
+	/// Description of SQLiteCommand.
+	/// </summary>
+	public class SQLiteCommand
+	{
+		public SQLiteCommand()
+		{
+		}
+	}
+}
